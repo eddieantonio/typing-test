6534454d4df7c8292da1b0157d77111a1eeaa585
Eddie Antonio Santos <easantos@ualberta.ca>
2019-10-16 16:17:02 -0600

    Starting to figure a few things out...

--- src/pages/TypingTest.svelte
+++ src/pages/TypingTest.svelte
@@ -4,9 +4,30 @@
   let buffer;
   let lastEvent;
 
+  let firstTimeFocused;
+
+  function recordFirstTimeFocused(event) {
+    let timestamp = performance.now()
+    console.assert(firstTimeFocused === undefined);
+    firstTimeFocused = timestamp;
+  }
+
   function recordKeypress(event) {
+    let timestamp = performance.now();
     let {data, inputType } = event;
-    lastEvent = JSON.stringify({event: inputType, char: data, timestamp: null }, null, 2);
+
+    // firstvoices creates inputType:
+    //  - "insertText"
+    //  - "deleteContentBackward" when deleting
+    // gboard creates inputType:
+    //  - "insertCompositionText" with the entire word. Even if you delete. This is annoying.
+    // keyman creates inputType:
+    //  - "insertText" for finals
+    //  - "insertText" for syllables, e.g., an insertText of ᐟ followed by ᐤ,
+    //      followed by ᐻ
+    //  - "deleteContentBackward"
+
+    lastEvent = JSON.stringify({event: inputType, char: data, timestamp }, null, 2);
     console.log({event});
   }
 </script>
@@ -14,6 +35,7 @@
 <p class="the-sentence">{sentence}</p>
 <textarea
   on:input={recordKeypress}
+  on:focus|once={recordFirstTimeFocused}
   class="type-syllabics"
   bind:value={buffer}
   placeholder="Tap here and start typing"
@@ -24,3 +46,4 @@
 />
 
 <pre style="white-space: pre-line;">{lastEvent}</pre>
+<output>{firstTimeFocused}</output>
