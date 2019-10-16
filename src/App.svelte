b45f6b4f3fbed8d9c4d75d5442325b34cb6f662c
Eddie Antonio Santos <easantos@ualberta.ca>
2019-10-16 09:54:43 -0600

    Store participant ID.

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,14 @@
-<p>Hello World</p>
+<script>
+let participantID;
+
+function nextScreen() {
+    localStorage.currentParticipantID = participantID;
+}
+</script>
+
+<form on:submit={nextScreen}>
+    <label for="partid"> Please enter your participant ID:
+        <input type="number" id="partid" name="participantID" bind:value={participantID} required>
+    </label>
+    <button type="submit"> Continue </button>
+</form>
