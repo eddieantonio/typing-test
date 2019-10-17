80f45cc5b13f04c8784e83045c4534b3041dbfc1
Eddie Antonio Santos <easantos@ualberta.ca>
2019-10-17 14:59:53 -0600

    Sort of get the routing correct?

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,49 @@
 <script>
-import {Router, Route} from 'svero';
+import {Router, Route, Link} from 'svero';
 
 import EnterParticipantID from './pages/EnterParticipantID.svelte';
+// TODO: rename this component:
 import BeforeTrial from './pages/BeforeTrial.svelte';
 import TypingTest from './pages/TypingTest.svelte';
-
-let sentences = [
-    {id: 1, name: 'tâpwe'},
-    {id: 2, name: 'miyo-kîsikâw'},
-];
 </script>
 
 <!-- https://github.com/kazzkiq/svero#usage -->
 <main>
   <Router>
-    <Route path="/" exact component={EnterParticipantID} />
-    <Route path="/trial" exact component={BeforeTrial} />
-    <Route path="/start-trial" exact component={TypingTest} />
+    <!-- Page for the researcher (i.e., me) -->
+    <Route path="/" exact component={EnterParticipantID} props={{
+      continueTo: '/instructions'
+    }} />
+
+    <!-- First page that participant sees -->
+    <Route path="/instructions" exact props={{
+      continueTo: '/layout'
+    }}>
+      <h1> TODO: instructions </h1>
+      <Link href="/layout">Continue</Link> 
+    </Route>
+
+    <!-- The researcher then selects a layout: -->
+    <Route path="/layout" exact component={BeforeTrial} />
+
+    <!-- The researcher hands it back and the instructions screen shows up -->
+    <Route path="/layout/:layout" exact>
+      <h1> TODO: instructions to do timed practice </h1>
+    </Route>
+
+    <Route path="/layout/:layout/practice" exact>
+      <h1> TODO: here, the participant practices for some time</h1>
+    </Route>
+
+    <Route path="/layout/:layout/sentence" exact>
+      <h1> TODO: the participant is asked if they want to begin the test proper. </h1>
+    </Route>
+
+    <Route path="/layout/:layout/sentence/:sentence_id" exact>
+      <h1> TODO: the participant is primed with the sentence for some time...  </h1>
+    </Route>
+
+    <!-- Then the participant is asked to type it exactly as it looks -->
+    <Route path="/layout/:layout/sentence/:sentence_id/type" exact component={TypingTest} />
   </Router>
 </main>
--- src/pages/EnterParticipantID.svelte
+++ src/pages/EnterParticipantID.svelte
@@ -1,14 +1,28 @@
 <script>
-import globals from '../globals';
+  import {navigateTo} from 'svero';
 
-let participantID = globals.currentParticipantID;
+  import globals from '../globals';
 
-function saveParticipantID() {
-  globals.currentParticipantID = participantID;
-}
+  // Props that come from the router.
+  export let router;
+  export let continueTo;
+
+  let participantID = globals.currentParticipantID;
+
+  console.assert(continueTo, 'expected a url to continue to but got nothing');
+
+  function continueToInstructions(event) {
+    event.preventDefault();
+    saveParticipantID();
+    navigateTo(continueTo);
+  }
+
+  function saveParticipantID() {
+    globals.currentParticipantID = participantID;
+  }
 </script>
 
-<form action="/trial" on:submit={saveParticipantID}>
+<form on:submit={continueToInstructions}>
   <label for="partid"> Please enter your participant ID:
       <input type="number" id="partid" bind:value={participantID} required>
   </label>
