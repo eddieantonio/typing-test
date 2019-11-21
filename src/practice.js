49b1d1c6e0fae3484d54a1297d90023a115a263d
Eddie Antonio Santos <easantos@ualberta.ca>
2019-11-21 12:31:01 -0700

    Commit practice words.

---
+++ src/practice.js
@@ -0,0 +1,15 @@
+/**
+ * Practice words
+ */
+export let PRACTICE_WORDS = [
+  "ᐱᒥᐸᔨᐤ",    // pimipayiw
+  "ᐁᑯᑌ",      // êkotê
+  "ᑮ ᐃᑖᐤ",    // kî-itâw
+  "ᐁ ᒪᐘᓱᔮᕽ",  // ê-mawasoyâhk
+  "ᓇᒨᔭ",      // namôya
+  "ᓂᐢᑯᒑᑳᐢ",   // miskocâkâs
+  "ᐴᒣᐤ",      // pômêw
+  "ᓇᒹᐨ",      // namwâc
+  "ᐱᔨᐢᐠ",     // piyisk
+  "ᒥᐢᑕᐦᐃ᙮"    // mistahi.
+];
